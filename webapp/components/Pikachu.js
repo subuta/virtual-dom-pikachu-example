f30226b299466f1825b4b5bc7daaae4b1606b060
shunta saito <s.shunta@goodpatch.com>
2016-08-21 14:39:28 +0900

    [modify]add watch and fix renderPikachu.

--- webapp/components/Pikachu.js
+++ webapp/components/Pikachu.js
@@ -1,4 +1,5 @@
 import h from 'snabbdom/h';
+import _ from 'lodash';
 
 import {connect, inject} from 'webapp/store.js'
 import {createSelector} from 'reselect';
@@ -22,16 +23,32 @@ const mapDispatchToProps = (dispatch) => {
   }
 };
 
+const getDotClass = (dot) => {
+  if (dot === 0) {
+    return '.dot--off';
+  }
+  return '.dot';
+};
+
+// render Pikachu from dots.
+const renderPikachu = (dots) => {
+  return h(`span.pikachu`, {}, _.map(dots, (row, rowIdx) => {
+    return h(`span.row.n${rowIdx}`, {}, _.map(row, (col, colIdx) => {
+      return h(`span.col.n${colIdx}${getDotClass(col)}`, {}, [String(col)]);
+    }));
+  }));
+};
+
 export default inject(({props}) => {
     console.log('[pikachu] rendered');
-    console.log(props);
+    console.log(renderPikachu(props.dots));
     return h(`span`, {
       on: {
         'click': function (ev) {
           return props.downA();
         }
       }
-    }, [JSON.stringify(props.dots)]);
+    }, [renderPikachu(props.dots)]);
   },
   mapStateToProps,
   mapDispatchToProps
---
+++ webapp/utils/generateStyle.js
@@ -0,0 +1,76 @@
+import postcssJs from 'postcss-js';
+import easings from 'postcss-easings';
+import autoprefixer from 'autoprefixer';
+import freeStyle from 'free-style';
+import i from 'icepick';
+import _ from 'lodash';
+import { paramCase } from 'change-case';
+
+// Create a container instance.
+const Style = freeStyle.create();
+let classes = {};
+let keyframes = {};
+
+export const addChangeListener = fn => Style.addChangeListener(fn);
+export const removeChangeListener = fn => Style.removeChangeListener(fn);
+
+export const resetStyles = () => {
+  classes = {};
+  keyframes = {};
+};
+
+// allow call register with null value.
+export const registerStyles = (styles = {}) => {
+  _.each(styles, (style, key) => {
+    registerStyle(key, style);
+  });
+  return classes;
+};
+
+const isStrict = false;
+export const registerStyle = (key, style) => {
+  if (isStrict && classes[key]) {
+    throw new Error(`style of ${key} is already registered!`);
+  }
+
+  let plugins = [
+    easings,
+    autoprefixer
+  ];
+
+  const postcss = postcssJs.sync(plugins);
+
+  const className = paramCase('my-' + key);
+  // inject style to free-style.
+  const uniqueKey = Style.registerStyle({
+    [`.${className}, &`]: postcss(style)
+  });
+
+  classes[key] = uniqueKey;
+  return uniqueKey;
+};
+
+export const registerKeyframes = (_keyframes) => {
+  _.each(_keyframes, (keyframe, key) => registerKeyframe(key, keyframe));
+  return keyframes;
+};
+
+export const registerKeyframe = (key, keyframe) => {
+  // inject keyframe to free-style.
+  const uniqueKey = Style.registerKeyframes(keyframe);
+  keyframes[key] = uniqueKey;
+  return uniqueKey;
+};
+
+export const registerRules = (rules) => {
+  _.each(rules, (rule, key) => registerRule(key, rule));
+};
+
+export const registerRule = (key, rule) => {
+  // inject rule to free-style.
+  Style.registerRule(key, rule);
+};
+
+export const getCSS = () => {
+  return Style.getStyles();
+};
